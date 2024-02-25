9dbd8e41085da5507d9f15e55c74c078aaa95489
veligoran1 <145706534+veligoran1@users.noreply.github.com>
2024-02-25 12:54:12 +0300

    Delete HW3/3_2.sql

--- HW3/3_2.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    EXTRACT(MONTH FROM t.transaction_date) AS month,
-    c.job_industry_category AS industry,
-    SUM(t.list_price) AS transaction_sum
-FROM transactions t
-JOIN customers c ON t.customer_id = c.customer_id
-GROUP BY month, industry
-ORDER BY month, industry;
